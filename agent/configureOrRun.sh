0250683af5f7cbe81fbbeb4f0eb6f8d1bdf0c2b5
Lincoln Zocateli <lincolnzocateli@hotmail.com>
2018-08-21 01:25:49 -0300

    Agent pode ser configurado ou apenas iniciado

---
+++ agent/configureOrRun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+work_dir="$DIR/_works/$(hostname)"
+
+if ! [ -d $work_dir ]; then
+  echo Executing agent config...
+  . $DIR/configureAndRun.sh
+;else
+  echo Executando background agent...
+  . $DIR/runAgent.sh
+fi
